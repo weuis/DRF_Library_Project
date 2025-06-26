46e964622c54ccc66ae80efb6864bd7451fc38ec
Алексей Розеган <rozeganalexey@gmail.com>
2025-06-26 17:56:37 +0200

    Start work of Users app

--- Great_Library/settings.py
+++ Great_Library/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "books",
+    "users",
     "rest_framework",
     "drf_spectacular",
 ]
@@ -145,3 +146,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.User'
